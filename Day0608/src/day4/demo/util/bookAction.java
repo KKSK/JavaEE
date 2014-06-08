856cb5468bcabfa29af4cd82f8546d09e62babb8
KKSK <dddlbj@163.com>
2014-06-08 16:48:09 +0800

    First

---
+++ Day0608/src/day4/demo/util/bookAction.java
@@ -0,0 +1,17 @@
+package day4.demo.util;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Administrator on 14-6-8.
+ */
+public class bookAction extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
